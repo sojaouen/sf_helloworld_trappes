97ef8cf9c75f5217f9a129fd72e6c6f003288970
Stéphane Jaouen <stephanejaouen@MacBook-Pro-M1-de-Stephane.local>
2021-04-13 12:25:14 +0200

    -

---
+++ src/Controller/LegalController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LegalController extends AbstractController
+{
+    /**
+     * @Route("/mentions-legales", name="legal")
+     */
+    public function index(): Response
+    {
+        return $this->render('legal/index.html.twig', [
+            'controller_name' => 'Mentions Légales',
+        ]);
+    }
+
+    /**
+     * @Route("/cookies", name="cookies")
+     */
+    public function cookies(): Response
+    {
+        return $this->render('legal/cookies.html.twig', [
+            'title' => "Politique concernant les cookies",
+            'content' => "Lorem ipsum.....Blablabla"
+        ]);
+    }
+
+    /**
+     * @Route("/cgu", name="cgu")
+     */
+    public function cgu(): Response
+    {
+        return $this->render('legal/index.html.twig', [
+            'controller_name' => 'Les CGU',
+        ]);
+    }
+}
